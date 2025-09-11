afd8df6b67ed82f779620756622ee07c05445e6e
SantiagoFerrusca1403 <santiagoalcarazf@gmail.com>
2025-09-11 10:17:56 -0600

    Asignación de variables y operaciones

--- JSParaPrincipiantes/App2.js
+++ JSParaPrincipiantes/App2.js
@@ -1,1 +1,14 @@
-console.log('This is a String')
+// asignaciones de valores a variables 
+var username = 'John';
+let lastUsername = 'Carter';
+//Cambio de nombre 
+username = 'Pedrito';
+//Valores constantes que no pueden cambiar 
+
+const PI = 3.1416;
+
+//Juntar nombre completo 
+let name = 'Santiago';
+let Lastname = 'Alcaraz'
+let CompleteName =  name + ' ' + lastname;
+console.log(CompleteName)
