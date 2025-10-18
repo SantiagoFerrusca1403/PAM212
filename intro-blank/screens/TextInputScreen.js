dd2071a608bc7dcf06aba804ed35b1db69bd4328
SantiagoFerrusca1403 <santiagoalcarazf@gmail.com>
2025-10-17 18:49:25 -0600

    TextInput & Alert: Completada
    Práctica 9

--- intro-blank/screens/TextInputScreen.js
+++ intro-blank/screens/TextInputScreen.js
@@ -1,13 +1,82 @@
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, TextInput, Alert, Button, StyleSheet } from 'react-native';
 
-export default function TextInputScreen() {
- 
-    return (
-      <View>
-        <Text>TextInput Proximamente...</Text>
-      </View>
-    )
+export default function TextScreen() {
+  const [nombre, setNombre] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  const [contrasenia, setContrasenia] = useState("");
+  const [comentario, setComentario] = useState("");
 
+  const enviarDatos = () => {
+    if (nombre.trim() === '' || contrasenia.trim() === "" ) {
+      Alert.alert('Error', 'Completa todos los campos');
+      alert('Error: Completa todos los campos');
+      setMensaje('Campo en blanco, por favor completa todos los campos');
+
+    } else {
+      Alert.alert('¡Hola!', 'Tus datos fueron enviados correctamente');
+      alert('¡Hola!', 'Tus datos fueron enviados correctamente.');
+
+      setMensaje('¡Hola!', 'Tus datos fueron enviados correctamente :3.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Práctica para ingresar tu nombre usando TextInput y Alert</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+        <TextInput
+        style={styles.input}
+        placeholder="Escribe tu constraseña"
+        value={contrasenia}
+        onChangeText={setContrasenia}
+        secureTextEntry={true}
+        keyboardType='numeric'
+      />
+        <TextInput
+        style={[styles.input, {height : 100, textAlignVertical: "top"}]}
+        placeholder="Escribe un comentario"
+        value={comentario}
+        onChangeText={setComentario}
+        multiline={true}
+        numberOfLines={4}
+      />
+
+      <Button title="Enviar" onPress={enviarDatos} />
+      <Text style={styles.mensaje}>{mensaje}</Text>
+    </View>    
+
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    padding:20,
+    gap:10
+  },
+  title:{
+    fontSize:25,
+    fontWeight:'bold'
+  },
+  input:{
+    width:'80%',
+    borderWidth:3,
+    borderColor:'#a47bf7ff',
+    padding:12,
+    borderRadius:9
+  },
+  mensaje:{
+    marginTop:20,
+    fontSize:18,
+    color:'#e431f1ff',
+    textAlign:'center'
+  }
+});
